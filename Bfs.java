25858d21dca30a894e4b540791da7c8e79900998
Waliullah <waliullah9252@gmail.com>
2024-09-07 00:36:09 +0600

    bfs travarsal

---
+++ Bfs.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class Bfs {
+    static final int N = 100007;
+    static Vector<Integer>[] vector = new Vector[N];
+    static boolean[] visited = new boolean[N];
+    static int[] level = new int[N];
+
+    public static void bfs(int s){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(s);
+        visited[s]=true;
+        level[s]=0;
+
+        while (!q.isEmpty()) {
+            int parent = q.poll();
+            System.out.println(parent);
+
+            for(int child: vector[parent]){
+                if(!visited[child]){
+                    q.add(child);
+                    visited[child] = true;
+                    level[child] = level[parent] + 1;
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your node: ");
+        int n = sc.nextInt();
+        System.out.print("Enter your edge: ");
+        int e = sc.nextInt();
+
+        for(int i=0; i<=n; i++){
+            vector[i] = new Vector<>();
+        }
+
+        for(int i=0; i<e; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            vector[a].add(b);
+            vector[b].add(a);
+        }
+
+        bfs(1);
+
+        System.out.println();
+        for(int i=0;i<=n; i++){
+            System.out.println("Node level "+i+" : "+level[i]);
+        }
+
+    }
+}
